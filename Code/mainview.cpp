287c137718a1e2a843b2feabdd43b5c68a2c3a3c
PJEilers <pieterjaneilers@gmail.com>
2018-02-14 14:52:11 +0100

    practice ex done

--- Code/mainview.cpp
+++ Code/mainview.cpp
@@ -1,8 +1,16 @@
+#define GL_GLEXT_PROTOTYPES
 #include "mainview.h"
 #include "math.h"
+#include "vertex.h"
 
 
 #include <QDateTime>
+#include <QSurfaceFormat>
+#include <QOpenGLShaderProgram>
+
+
+
+
 
 MainView::MainView(QWidget *parent) : QOpenGLWidget(parent) {
     qDebug() << "MainView constructor";
@@ -14,9 +22,16 @@ MainView::~MainView() {
     debugLogger->stopLogging();
 
     qDebug() << "MainView destructor";
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
 }
 
 void MainView::initializeGL() {
+
+
+
+
+
     qDebug() << ":: Initializing OpenGL";
     initializeOpenGLFunctions();
 
@@ -32,6 +47,45 @@ void MainView::initializeGL() {
     QString glVersion;
     glVersion = reinterpret_cast<const char*>(glGetString(GL_VERSION));
     qDebug() << ":: Using OpenGL" << qPrintable(glVersion);
+
+    QOpenGLShaderProgram program;
+    program.addShaderFromSourceFile(QOpenGLShader::Vertex, ":/shaders/vertshader.glsl");
+    program.addShaderFromSourceFile(QOpenGLShader::Fragment, ":/shaders/fragshader.glsl");
+    program.link();
+    program.bind();
+
+    vertex vArray[3];
+    vArray[0].x=-1;
+    vArray[0].y=-1;
+    vArray[0].r=1;
+    vArray[0].g=0;
+    vArray[0].b=0;
+    vArray[1].x=1;
+    vArray[1].y=-1;
+    vArray[1].r=0;
+    vArray[1].g=1;
+    vArray[1].b=0;
+    vArray[2].x=0;
+    vArray[2].y=1;
+    vArray[2].r=0;
+    vArray[2].g=0;
+    vArray[2].b=1;
+
+
+
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex)*3, &vArray[0].x, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+
+    #define BUFFER_OFFSET(i) ((char *)NULL + (i))
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), BUFFER_OFFSET(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), BUFFER_OFFSET(8));
 }
 
 void MainView::resizeGL(int newWidth, int newHeight) {
@@ -41,7 +95,9 @@ void MainView::resizeGL(int newWidth, int newHeight) {
 }
 
 void MainView::paintGL() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glDrawArrays(GL_TRIANGLES, 0,3);
 }
 
 void MainView::onMessageLogged( QOpenGLDebugMessage Message ) {
