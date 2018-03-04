8bf0ba85a20d252a03573d05ffbe104b17ee0ab6
lorenzoamabili <lorenzo.amabili@gmail.com>
2018-03-04 17:26:57 +0100

    Add files via upload

---
+++ mainview.h
@@ -0,0 +1,111 @@
+#ifndef MAINVIEW_H
+#define MAINVIEW_H
+
+#include "model.h"
+
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLDebugLogger>
+#include <QOpenGLShaderProgram>
+#include <QTimer>
+#include <QVector3D>
+#include <QMatrix4x4>
+#include <memory>
+
+
+class MainView : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core {
+    Q_OBJECT
+
+    QOpenGLDebugLogger *debugLogger;
+    QTimer timer; // timer used for animation
+
+    QOpenGLShaderProgram shaderProgramNormal;
+    QOpenGLShaderProgram shaderProgramGouraud;
+    QOpenGLShaderProgram shaderProgramPhong;
+
+    GLint uniformModelViewTransformPhong;
+    GLint uniformProjectionTransformPhong;
+    GLint uniformNormalMatrixPhong;
+    GLint uniformLightPositionPhong;
+    GLint uniformMaterialStatsPhong;
+    GLint uniformMaterialColorPhong;
+    GLint uniformSampler2DPhong;
+
+    GLint uniformModelViewTransformGouraud;
+    GLint uniformProjectionTransformGouraud;
+    GLint uniformNormalMatrixGouraud;
+    GLint uniformLightPositionGouraud;
+    GLint uniformMaterialStatsGouraud;
+    GLint uniformMaterialColorGouraud;
+    GLint uniformSampler2DGouraud;
+
+    GLint uniformModelViewTransformNormal;
+    GLint uniformProjectionTransformNormal;
+    GLint uniformNormalMatrixNormal;
+
+    GLuint tex;
+
+    // Mesh values
+    GLuint meshVAO;
+    GLuint meshVBO;
+    GLuint meshSize;
+    QMatrix4x4 meshTransform;
+    QMatrix4x4 lightTransform;
+
+    // Transforms
+    float scale = 1.f;
+    QVector3D rotation;
+    QMatrix4x4 projectionTransform;
+
+
+
+public:
+    enum ShadingMode : GLuint
+    {
+        PHONG = 0, NORMAL, GOURAUD
+    };
+
+    ShadingMode currentShading = PHONG;
+
+    MainView(QWidget *parent = 0);
+    ~MainView();
+
+    // Functions for widget input events
+    void setRotation(int rotateX, int rotateY, int rotateZ);
+    void setScale(int scale);
+    void setShadingMode(ShadingMode shading);
+
+    QVector<quint8> imageToBytes(QImage image);
+
+protected:
+    void initializeGL();
+    void resizeGL(int newWidth, int newHeight);
+    void paintGL();
+
+    // Functions for keyboard input events
+    void keyPressEvent(QKeyEvent *ev);
+    void keyReleaseEvent(QKeyEvent *ev);
+
+    // Function for mouse input events
+    void mouseDoubleClickEvent(QMouseEvent *ev);
+    void mouseMoveEvent(QMouseEvent *ev);
+    void mousePressEvent(QMouseEvent *ev);
+    void mouseReleaseEvent(QMouseEvent *ev);
+    void wheelEvent(QWheelEvent *ev);
+
+private slots:
+    void onMessageLogged( QOpenGLDebugMessage Message );
+
+private:
+    void createShaderProgram();
+    void loadMesh();
+
+    void destroyModelBuffers();
+
+    void updateProjectionTransform();
+    void updateModelTransforms();
+};
+
+#endif // MAINVIEW_H
